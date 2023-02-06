506c54e0772e4b41ba179a19656fb2dd37d6214f
Christopher Álvarez <christopherjavier1998@gmail.com>
2023-02-06 19:32:10 +0000

    Domain generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  const pronoun = ["the", "our"];
+  const adj = ["great", "big"];
+  const noun = ["jogger", "racoon"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    console.log(pronoun[i]);
+  }
+  for (let i = 0; i < adj.length; i++) {
+    console.log(adj[i]);
+  }
+  for (let i = 0; i < noun.length; i++) {
+    console.log(noun[i]);
+  }
+
+  const aleatorio = () => {};
 };
