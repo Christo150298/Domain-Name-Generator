9cfff1c41a1b68f41497a117d84b7fab488960f6
Christopher Álvarez <christopherjavier1998@gmail.com>
2023-02-15 12:34:54 +0000

    nuevos doominios

--- src/app.js
+++ src/app.js
@@ -8,25 +8,27 @@ window.onload = () => {
   let pronoun = ["the", "our"];
   let adj = ["great", "big"];
   let noun = ["jogger", "racoon"];
-
-  function generadorAleatorio(param1, param2, param3) {
+  let domain = [".com", ".net", ".io", ".us", ".es"];
+  function generadorAleatorio(param1, param2, param3, param4) {
     for (let a = 0; a < param1.length; a++) {
       for (let b = 0; b < param2.length; b++) {
         for (let c = 0; c < param3.length; c++) {
-          let nuevoNombre = document.createTextNode(
-            param1[a] + param2[b] + param3[c] + ".com"
-          );
-          let lista = document.querySelector("#listaNombresDominios");
-          let elemento = document.createElement("div");
-          elemento.className = "alert alert-dark";
-          lista.appendChild(elemento);
-          elemento.appendChild(nuevoNombre);
+          for (let d = 0; d < param4.length; d++) {
+            let nuevoNombre = document.createTextNode(
+              param1[a] + param2[b] + param3[c] + param4[d]
+            );
+            let lista = document.querySelector("#listaNombresDominios");
+            let elemento = document.createElement("div");
+            elemento.className = "alert alert-dark";
+            lista.appendChild(elemento);
+            elemento.appendChild(nuevoNombre);
+          }
         }
       }
     }
   }
   document.querySelector("#boton").addEventListener("click", () => {
-    generadorAleatorio(pronoun, adj, noun);
+    generadorAleatorio(pronoun, adj, noun, domain);
     document.querySelector("#boton").style.display = "none";
   });
 };
